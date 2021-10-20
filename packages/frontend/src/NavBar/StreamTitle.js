42fcb8b22663a71f5536aa08d08bd2612e3596fb
buzz <buzz@users.noreply.github.com>
2021-10-21 01:11:27 +0200

    fix: fix stream online display

--- packages/frontend/src/NavBar/StreamTitle.js
+++ packages/frontend/src/NavBar/StreamTitle.js
@@ -2,22 +2,22 @@ import { useEffect, useRef, useState } from 'react'
 
 import style from './NavBar.sss'
 
-const getTimeSince = (duration) => {
-  let dur = duration
+const getTimeSince = (startTime) => {
+  let duration = Date.now() - startTime
   const portions = []
 
   const msInHour = 1000 * 60 * 60
-  const hours = Math.trunc(dur / msInHour)
+  const hours = Math.trunc(duration / msInHour)
   if (hours > 0) {
     portions.push(`${hours} h`)
-    dur -= hours * msInHour
+    duration -= hours * msInHour
   }
 
   const msInMinute = 1000 * 60
-  const minutes = Math.trunc(dur / msInMinute)
+  const minutes = Math.trunc(duration / msInMinute)
   if (minutes > 0) {
     portions.push(`${minutes} min`)
-    dur -= minutes * msInMinute
+    duration -= minutes * msInMinute
   }
 
   return portions.length ? portions.join(' ') : 'less than 1 min'
@@ -25,14 +25,12 @@ const getTimeSince = (duration) => {
 
 const StreamTitle = ({ streamInfo: { streamOnline, streamStart, streamTitle } }) => {
   let statusText
-  let timeDiff
 
   const [timeSince, setTimeSince] = useState('')
   const intervalRef = useRef()
 
   if (streamOnline === 'online') {
     statusText = streamTitle || 'No name stream'
-    timeDiff = Date.now() - streamStart
   } else if (streamOnline === 'offline') {
     statusText = 'Stream offline'
   } else {
@@ -40,19 +38,24 @@ const StreamTitle = ({ streamInfo: { streamOnline, streamStart, streamTitle } })
   }
 
   useEffect(() => {
-    setTimeSince(getTimeSince(timeDiff))
-    intervalRef.current = window.setInterval(() => setTimeSince(getTimeSince(timeDiff)), 1000 * 60)
+    setTimeSince(getTimeSince(streamStart))
+    intervalRef.current = window.setInterval(() => {
+      setTimeSince(getTimeSince(streamStart))
+    }, 1000 * 5)
+
     return () => {
       if (intervalRef.current) {
         window.clearInterval(intervalRef.current)
       }
     }
-  }, [timeDiff])
+  }, [streamStart])
+
+  const timeSinceLabel = streamOnline === 'online' ? `(online for ${timeSince})` : undefined
 
   return (
     <div>
       {statusText}
-      <span className={style.onlineSince}>(online since {timeSince})</span>
+      <span className={style.onlineSince}>{timeSinceLabel}</span>
     </div>
   )
 }
