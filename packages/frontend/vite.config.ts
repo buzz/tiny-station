af4a8b20a59fce9b16e7766b699c206d00e9a4a2
buzz <buzz@users.noreply.github.com>
2026-01-28 12:55:30 +0100

    chore(frontend): use vite-tsconfig-paths

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,10 +2,11 @@ import path from 'node:path'
 
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths({ projects: ['tsconfig.app.json'] }), react()],
   server: {
     port: 3000,
     proxy: {
