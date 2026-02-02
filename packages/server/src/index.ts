032d51113cc6b74edddea33255c57dbb0ff2dddc
buzz <buzz@users.noreply.github.com>
2026-02-02 17:53:56 +0100

    chore(server): listen on all interfaces

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,12 +2,12 @@ import process from 'node:process'
 
 import makeApp from '#app.js'
 
-import { PORT } from './constants.js'
+import { HOST, PORT } from './constants.js'
 
 const isDebug = process.env.NODE_ENV !== 'production'
 const fastify = makeApp(isDebug)
 
-fastify.listen({ port: PORT }, (err, address) => {
+fastify.listen({ host: HOST, port: PORT }, (err, address) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
